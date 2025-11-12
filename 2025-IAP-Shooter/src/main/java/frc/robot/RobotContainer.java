e8a439fe6572e2deff23fc8539b278570cbcf9c7
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2025-11-12 14:51:39 -0800

    Update RobotContainer to use VelocityMeasurement
    Replaced ExampleSubsystem with VelocityMeasurement in RobotContainer.

--- 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
+++ 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,12 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+//Import Subsystems
+import frc.robot.subsystems.VelocityMeasurement;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,8 +21,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  // The robot's subsystems and commands are defined here
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final VelocityMeasurement velocityMeasurement = new VelocityMeasurement();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =

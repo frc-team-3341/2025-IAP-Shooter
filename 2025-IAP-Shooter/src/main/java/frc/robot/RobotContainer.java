967845249d6b23ef79892619f32ec71e849b9de5
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2025-11-15 15:00:51 -0800

    Update RobotContainer.java

--- 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
+++ 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -46,12 +46,12 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    //new Trigger(m_exampleSubsystem::exampleCondition)
+    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -61,6 +61,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(velocityMeasurment);
+    //return Autos.exampleAuto(velocityMeasurment);
   }
 }

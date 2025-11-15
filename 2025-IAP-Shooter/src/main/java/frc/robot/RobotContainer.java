39a3b2b0468f36ed08e8110e97eaa3305e91e8c4
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2025-11-15 14:54:37 -0800

    Update RobotContainer.java

--- 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
+++ 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 //Import Subsystems
-import frc.robot.subsystems.VelocityMeasurement;
+import frc.robot.subsystems.VelocityMeasurment;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,7 +23,7 @@ import frc.robot.subsystems.VelocityMeasurement;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final VelocityMeasurement velocityMeasurement = new VelocityMeasurement();
+  private final VelocityMeasurment velocityMeasurement = new VelocityMeasurment();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =

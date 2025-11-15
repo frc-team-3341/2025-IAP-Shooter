78dec2b8c076ce4bb46c3db599cd1d2f75b32d88
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2025-11-15 15:06:21 -0800

    Refactor RobotContainer to include ExampleSubsystem

--- 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
+++ 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,10 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-
-//Import Subsystems
-import frc.robot.subsystems.VelocityMeasurment;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,10 +18,10 @@ import frc.robot.subsystems.VelocityMeasurment;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
+@SuppressWarnings("unused")
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final VelocityMeasurment velocityMeasurement = new VelocityMeasurment();
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -46,12 +44,12 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //new Trigger(m_exampleSubsystem::exampleCondition)
-    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -60,7 +58,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    //return Autos.exampleAuto(velocityMeasurment);
+     //An example command will be run in autonomous
+    return Autos.exampleAuto(velocityMeasurment);
   }
 }

61479b060097371c8335262c85b87d20dc22da2d
Me <ryanh41137@gmail.com>
2025-12-06 13:15:10 -0800

    Import velocity measurement to robot controller

--- 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
+++ 2025-IAP-Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.VelocityMeasurment;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -23,6 +24,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final VelocityMeasurment m_velocityMeasurment = new VelocityMeasurment();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
